d9ca232ced52948096b828a3ab166c912b6e7dd0
Amanda Williamson <afransue@gmail.com>
2022-06-18 21:32:48 -0400

    updated solution

--- variables/solution_5.rb
+++ variables/solution_5.rb
@@ -3,3 +3,6 @@ Look at the example programs provided.
 What does x print to the screen in each case? Do they both give errors? 
 Are the errors different? Why?
 =end
+
+#The first program will print the number 3.
+#The second program will throw an error because x is only available in the do/end method. 
